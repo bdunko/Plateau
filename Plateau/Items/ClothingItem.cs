73fa8fc7e0a65f25c76180393ec33c6533307520
bdunko <bdunko@gmail.com>
2022-11-26 18:40:50 -0500

    improved clothing recolors

--- Plateau/Items/ClothingItem.cs
+++ Plateau/Items/ClothingItem.cs
@@ -37,13 +37,13 @@ namespace Plateau.Items
 
         private Util.RecolorAdjustment GetRecolorAdjustment()
         {
-            if(this.HasTag(Tag.SHIRT) || this.HasTag(Tag.SOCKS))
+            if(this.HasTag(Tag.SHIRT) || this.HasTag(Tag.GLOVES) || this.HasTag(Tag.SHOES))
             {
                 return Util.RecolorAdjustment.DARKEN;
-            } else if (this.HasTag(Tag.SCARF) || this.HasTag(Tag.SAILCLOTH) || this.HasTag(Tag.GLOVES))
+            } else if (this.HasTag(Tag.SCARF) || this.HasTag(Tag.SAILCLOTH) )
             {
                 return Util.RecolorAdjustment.LIGHTEN;
-            } else if (this.HasTag(Tag.OUTERWEAR))
+            } else if (this.HasTag(Tag.OUTERWEAR) || this.HasTag(Tag.SOCKS))
             {
                 return Util.RecolorAdjustment.SLIGHT_LIGHTEN;
             }
